eea00688fc842347c2070cf7263de501b2df0d78
Sofroniy <Sofroniy.m@gmail.com>
2020-04-06 19:17:14 +0300

    feat: implement user api

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,27 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const userModel = require('./user.model');
+
+let data = [];
+
+const getAll = async () => data;
+const getUserById = async id => data.find(i => i.id === id);
+const deleteUser = async id => (data = data.filter(i => i.id !== id));
+
+const createUser = async user => {
+  const newUser = new userModel(user);
+  data.push(newUser);
+  return newUser;
 };
 
-module.exports = { getAll };
+const updateUser = async (id, newData) => {
+  const idx = data.findIndex(i => i.id === id);
+  Object.assign(data[idx], newData);
+  return data[idx];
+};
+
+module.exports = {
+  getAll,
+  createUser,
+  getUserById,
+  deleteUser,
+  updateUser
+};
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,28 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+  return res.status(200).json(users.map(User.toResponse));
+});
+
+router.route('/:id').get(async (req, res) => {
+  const user = await usersService.getUserById(req.params.id);
+  if (!user) return res.status(404).json({ err: 'User not found' });
+  return res.status(200).json(User.toResponse(user));
+});
+
+router.route('/').post(async (req, res) => {
+  const user = await usersService.createUser(req.body);
+  return res.status(200).json(User.toResponse(user));
+});
+
+router.route('/:id').put(async (req, res) => {
+  const user = await usersService.updateUser(req.params.id, req.body);
+  return res.json(user);
+});
+
+router.route('/:id').delete(async (req, res) => {
+  const user = await usersService.deleteUser(req.params.id);
+  return res.json(user);
 });
 
 module.exports = router;
